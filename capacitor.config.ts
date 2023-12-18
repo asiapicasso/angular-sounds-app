6d025c0df34ccb8c4b6e2f67ce2b4b4a523bcc45
asiapicasso <asia.picasso@gmail.com>
2023-12-18 10:41:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-sounds-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
