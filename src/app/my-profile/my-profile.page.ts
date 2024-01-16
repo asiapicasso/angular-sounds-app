03b6c4279e28db978a244a102cf66f3be8e2ef53
asiapicasso <asia.picasso@gmail.com>
2024-01-16 15:30:55 +0100

    generate page profile. ok.

--- src/app/my-profile/my-profile.page.ts
+++ src/app/my-profile/my-profile.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
@@ -12,9 +14,35 @@ import { IonicModule } from '@ionic/angular';
 })
 export class MyProfilePage implements OnInit {
 
-  constructor() { }
+  user: any;
+  vibrations: string[] = [];
+
+  constructor(private userService: UserService, private navCtrl: NavController, private router: Router) { }
 
   ngOnInit() {
+    this.user = {
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+    };
+    this.generateVibrations();
+
+  }
+
+  editProfile() {
+    // Redirect to the profile update page
+    this.navCtrl.navigateForward('/profile/update'); //route d'exemple
+  }
+
+  navigateToMyVibrationsPage() {
+    this.router.navigate(['/my-vibrations']);
+  }
+
+  generateVibrations() {
+    const count = this.vibrations.length + 1;
+    for (let i = 0; i < 5; i++) {
+      this.vibrations.push(`My Vibration ${count + i}`);
+    }
   }
 
 }
