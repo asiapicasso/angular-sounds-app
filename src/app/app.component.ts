4669d437d3abcd53bc7a48bbc7305ea016019483
asiapicasso <asia.picasso@gmail.com>
2024-01-22 13:48:46 +0100

    add log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonHeader } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { homeOutline, homeSharp, headsetOutline, headsetSharp, leafOutline, leafSharp, personOutline, personSharp, settingsOutline, settingsSharp } from 'ionicons/icons';
+import { AuthService } from './security/auth.service';
+import { IonicModule } from '@ionic/angular';
 
 
 @Component({
@@ -11,7 +13,7 @@ import { homeOutline, homeSharp, headsetOutline, headsetSharp, leafOutline, leaf
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet],
+  imports: [IonicModule, RouterLink, RouterLinkActive, CommonModule],
 })
 export class AppComponent {
   public appPages = [
@@ -19,12 +21,22 @@ export class AppComponent {
     { title: 'My vibrations', url: '/my-vibrations', icon: 'headset' },
     { title: 'The Plants', url: '/the-plants', icon: 'leaf' },
     { title: 'My profile', url: '/my-profile', icon: 'settings' },
-    { title: 'Connection', url: '/auth', icon: 'person' },
+    //{ title: 'Log Out', url: '/auth/logout', icon: 'person' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {
-    addIcons({ homeOutline, headsetOutline, leafOutline, settingsOutline, settingsSharp, personOutline, personSharp, leafSharp, headsetSharp, homeSharp });
 
+
+  constructor(private authService: AuthService) {
+    addIcons({ homeOutline, headsetOutline, leafOutline, settingsOutline, settingsSharp, personOutline, personSharp, leafSharp, headsetSharp, homeSharp });
+  }
+
+  logOut() {
+    //TODO deconnection
+    //this.authService.logout().subscribe(() => {
+
+    // this.router.navigate(['/login']);
+    //});
+    console.log("logging out");
   }
 
 }
